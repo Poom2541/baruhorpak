4d699a3752246aab1fffc333e9b802dc5df0363b
Poom <poomfilm2541@gmail.com>
2020-12-07 20:02:27 +0700

    เพิ่ม ตั้งค่าfirebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
-import 'package:bsruhorpak/register.dart';
+
+import 'package:bsruhorpak/resigter.dart';
+import 'package:bsruhorpak/singup.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:bsruhorpak/splashscreen.dart';
+import 'package:bsruhorpak/login.dart';
+
+// import 'package:bsruhorpak/splashscreen.dart';
 // import 'package:bsruhorpak/home.dart';
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
     runApp(MyApp());}
 class MyApp extends StatelessWidget {
@@ -17,13 +24,17 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/':(_) => SplashScreen(),
-        // '/login':(_) => RegisterPage(),
-        // '/home':(_) => HomePage(), 
+        '/':(_) => Login(),
+        // '/login':(_) => LoginPage(),
         '/register':(_) => RegisterPage(),
+        '/singup':(_ )=> MySignUpPage()
+        // '/register':(_) => RegisterPage(),
         },
     );
   }
+}
+
+class LoginPage {
 }
 
 
